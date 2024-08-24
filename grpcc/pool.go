aeb5e5a22c85f330a21d7ecfd08957b002337334
gongyulin <gongyulin548@hellobike.com>
2024-08-24 20:23:36 +0800

    feat: grpc连接参数

--- grpcc/pool.go
+++ grpcc/pool.go
@@ -17,11 +17,7 @@ func PoolFactoryOut(cls *cluster.Cluster, ins *cluster.Instance) (interface{}, e
 	// todo get service info/config
 	p := shared.NewPool(1, 1, 9999, time.Minute*1, func() (interface{}, error) {
 		ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-		cc, err := grpc.DialContext(ctx, ins.IP+":"+strconv.Itoa(ins.Port), grpc.WithCodec(codec.Codec()), grpc.WithInsecure(), grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                10 * time.Second,
-			Timeout:             3 * time.Second,
-			PermitWithoutStream: true, // 允许没有活跃流的心跳
-		}))
+		cc, err := grpc.DialContext(ctx, ins.IP+":"+strconv.Itoa(ins.Port), grpc.WithCodec(codec.Codec()), grpc.WithInsecure())
 		if err != nil {
 			return nil, err
 		}
@@ -53,3 +49,9 @@ func PoolFactoryIn(ins *localInstance.LocalInstance) (interface{}, error) {
 	})
 	return p, nil
 }
+
+//grpc.WithKeepaliveParams(keepalive.ClientParameters{
+//Time:                10 * time.Second,
+//Timeout:             3 * time.Second,
+//PermitWithoutStream: true, // 允许没有活跃流的心跳
+//})
