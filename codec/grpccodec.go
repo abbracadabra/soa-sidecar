de13050435263fd1a3b4ba8adda77d9b43860283
gongyulin <gongyulin548@hellobike.com>
2024-08-29 02:21:56 +0800

    feat: grpc直接透传字节数据，无需protobuf编解码

--- codec/grpccodec.go
+++ codec/grpccodec.go
@@ -9,57 +9,52 @@ import (
 
 // proto编解码器
 func Codec() grpc.Codec {
-	return &rawCodec{&protoCodec{}}
-	//return CodecWithParent(&protoCodec{})
+	return &rawCodec{}
 }
-
-//func CodecWithParent(fallback grpc.Codec) grpc.Codec {
-//	return &rawCodec{fallback}
-//}
 
 type rawCodec struct {
-	parentCodec grpc.Codec
 }
-
-//type myMessage []byte
-
-//type myMessage struct {
-//	payload []byte
-//}
 
 func (c *rawCodec) Marshal(v interface{}) ([]byte, error) {
 	out, ok := v.([]byte)
-	if !ok {
-		fmt.Println("use default codec")
-		return c.parentCodec.Marshal(v)
+	if ok {
+		return out, nil
 	}
-	fmt.Println("use raw payload")
-	return out, nil
+	pt, ok := v.(proto.Message)
+	if ok {
+		return proto.Marshal(pt)
+	}
+	return nil, fmt.Errorf("unsupported message type: %T", v)
 }
 
 func (c *rawCodec) Unmarshal(data []byte, v interface{}) error {
 	dst, ok := v.(*[]byte)
-	if !ok {
-		return c.parentCodec.Unmarshal(data, v)
+	if ok {
+		*dst = data
+		return nil
 	}
-	*dst = data
-	return nil
+	pt, ok := v.(proto.Message)
+	if ok {
+		return proto.Unmarshal(data, pt)
+	}
+	return fmt.Errorf("unsupported message type: %T", v)
 }
 
 func (c *rawCodec) String() string {
-	return fmt.Sprintf("proxy>%s", c.parentCodec.String())
+	return fmt.Sprintf("proxy codec")
 }
 
-type protoCodec struct{}
-
-func (protoCodec) Marshal(v interface{}) ([]byte, error) {
-	return proto.Marshal(v.(proto.Message))
-}
-
-func (protoCodec) Unmarshal(data []byte, v interface{}) error {
-	return proto.Unmarshal(data, v.(proto.Message))
-}
-
-func (protoCodec) String() string {
-	return "proto"
-}
+//
+//type protoCodec struct{}
+//
+//func (protoCodec) Marshal(v interface{}) ([]byte, error) {
+//	return proto.Marshal(v.(proto.Message))
+//}
+//
+//func (protoCodec) Unmarshal(data []byte, v interface{}) error {
+//	return proto.Unmarshal(data, v.(proto.Message))
+//}
+//
+//func (protoCodec) String() string {
+//	return "proto"
+//}
